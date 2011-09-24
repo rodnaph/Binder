c7747f792f5b6b9d41f17cd9076620e6498ee286
rod <rod@pu-gh.com>
2011-09-24 01:02:35 +0100

    added README

---
+++ README.markdown
@@ -0,0 +1,60 @@
+
+Binder
+====
+
+Binder is a simple module for binding abilities together between Javascript objects.  It takes the nouns out of dependency injection and ties functions together between objects in a lightweight, decoupled manner.
+
+Contrived Example
+----------------
+
+Objects specify their abilities using the _can_ property array, and they specify abilities they require using the _needs_ property array.
+
+<pre>
+Car.prototype = {
+  can: [ 'driveToWork' ],
+  needs: [ 'goForwards', 'turnLeft', 'turnRight' ],
+  // ...
+};
+</pre>
+
+Here, the car advertises it has the ability to drive you to work, but that it needs to collaborate with objects that can help it go forwards, turn left, and turn right.  This kind of object would then want to collaborate with something like an engine...
+
+<pre>
+Engine.prototype = {
+  can: [ 'goForwards', 'goBackwards' ],
+  // ...
+};
+</pre>
+
+Usage Example
+-------------
+
+To use Binder you can install it through _npm_.
+
+<pre>
+var binder = require( 'binder' );
+</pre>
+
+It then takes functions to construct objects as the single argument to its _make_ method.
+
+<pre>
+binder.make( Engine );
+binder.make( SteeringWheel );
+var car = binder.make( Car );
+</pre>
+
+The car will then be able to use the abilities that were provided by the engine and the steering wheel.
+
+<pre>
+Car.prototype = {
+  // ...
+  driveToWork: function() {
+    this.goForwards();
+    this.turnLeft();
+    // etc...
+  }
+};
+</pre>
+
+The car is then communicating with its collaborators without being dependent on _who_ they are, the only coupling is by the specific abilities that it requires.
+
--- index.js
+++ index.js
@@ -1,79 +1,67 @@
 
+var abilities = {};
+
 /**
- * The binder creates objects, taking abilities they can() do, and
- * giving them abilities they needs()
+ * Stores an ability that an object has.  If another object has
+ * already defined this ability an error is thrown.
  *
+ * @param Object object
+ * @param String can
  */
-exports = {
+function storeAbility( object, can ) {
 
-    /**
-     * @var Object Store for ability/object map
-     */
-    abilities: {},
-
-    /**
-     * Given a function for creating an object, create it and
-     * give it all the verbs it needs to work.
-     *
-     * @param Function construct
-     *
-     * @return Array
-     */
-    make: function( construct ) {
-
-        var object = new construct();
-        var needs = object.needs || [];
-        var cans = object.can || [];
-
-        for ( var i=0; i<cans.length; i++ ) {
-            this.storeAbility( object, cans[i] );
-        }
-
-        for ( var i=0; i<needs.length; i++ ) {
-            this.giveAbility( object, needs[i] );
-        }
-
-        return object;
-
-    },
-
-    /**
-     * Stores an ability that an object has.  If another object has
-     * already defined this ability an error is thrown.
-     *
-     * @param Object object
-     * @param String can
-     */
-    storeAbility: function( object, can ) {
-
-        if ( this.abilities[can] ) {
-            throw 'Ability "' +can+ '" already provided';
-        }
-
-        this.abilities[ can ] = object;
-
-    },
-
-    /**
-     * Tries to give the object the ability it needs.  If the ability
-     * has not been provided yet then an error is thrown.
-     *
-     * @param Object object
-     * @param String need
-     */
-    giveAbility: function( object, need ) {
-
-        if ( !this.abilities[need] ) {
-            throw 'Ability "' +need+ '" has not been defined';
-        }
-
-        var ability = this.abilities[ need ];
-
-        object[ need ] = function() {
-            return ability[ need ].apply( ability, arguments );
-        };
-
+    if ( abilities[can] ) {
+        throw 'Ability "' +can+ '" already provided';
     }
+
+    abilities[ can ] = object;
+
+}
+
+/**
+ * Tries to give the object the ability it needs.  If the ability
+ * has not been provided yet then an error is thrown.
+ *
+ * @param Object object
+ * @param String need
+ */
+function giveAbility( object, need ) {
+
+    if ( !abilities[need] ) {
+        throw 'Ability "' +need+ '" has not been defined';
+    }
+
+    var ability = abilities[ need ];
+
+    object[ need ] = function() {
+        return ability[ need ].apply( ability, arguments );
+    };
+
+}
+
+/**
+ * Given a function for creating an object, create it and
+ * give it all the verbs it needs to work.
+ *
+ * @param Function construct
+ *
+ * @return Array
+ */
+exports.make = function( construct ) {
+
+    var object = new construct();
+    var needs = object.needs || [];
+    var cans = object.can || [];
+
+    for ( var i=0; i<cans.length; i++ ) {
+        storeAbility( object, cans[i] );
+    }
+
+    for ( var i=0; i<needs.length; i++ ) {
+        giveAbility( object, needs[i] );
+    }
+
+    return object;
 
 };
 
