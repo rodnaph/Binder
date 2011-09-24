9fbaeb271ce55d05f46007ed8cd46472a5f56f74
rod <rod@pu-gh.com>
2011-09-24 21:10:33 +0100

    added tests for exceptions thrown

--- test.js
+++ test.js
@@ -11,6 +11,12 @@ function Engine() {}
 
 function Car() {}
 
+function ClassicCar() {}
+
+function Lorry() {}
+
+function Minibus() {}
+
 Engine.prototype = {
     can: [ 'goForwards', 'goBackwards' ],
     goForwards: function() {},
@@ -26,6 +32,20 @@ Car.prototype = {
     }
 };
 
+ClassicCar.prototype = {
+    can: [ 'driveToWork' ]
+};
+
+Lorry.prototype = {
+    needs: [ 'loadWithLego' ]
+};
+
+Minibus.prototype = {
+    can: [ 'getPeople' ]
+};
+
+////////////////////////////////////////////////////////////////////
+
 runTest( 'Binder exposes its own abilities', function() {
     var b = binder.create();
     assert.ok( b.binderMake, 'Binder did not advertise own abilities' );
@@ -37,5 +57,27 @@ runTest( 'Binder exposes abilities of created objects', function() {
     var car = b.make( Car );
     assert.ok( car.goForwards, 'goForwards should have been bound from Engine' );
     assert.ok( car.goBackwards, 'goBackwards should have been bound from Engine' );
+});
+
+runTest( 'Exception thrown when ability not met', function() {
+    var b = binder.create();
+    assert.throws(function() {
+        b.make( Lorry );
+    });
+});
+
+runTest( 'Exception thrown when claimed ability does not exist', function() {
+    var b = binder.create();
+    assert.throws(function() {
+        b.make( Minibus );
+    });
+});
+
+runTest( 'Exception thrown when ability is redefined', function() {
+    var b = binder.create();
+    assert.throws(function() {
+        b.make( Car );
+        b.make( ClassicCar );
+    });
 });
 
